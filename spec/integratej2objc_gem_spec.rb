c66ce6bdaba06bdb47b357242b461a97316fcb66
Jon Nolen <jon.nolen@gmail.com>
2015-02-06 21:50:44 -0500

    working on test driving changes, starting with correctly testing the existing script happy path.

--- spec/integratej2objc_gem_spec.rb
+++ spec/integratej2objc_gem_spec.rb
@@ -1,9 +1,44 @@
 require 'spec_helper'
 
+describe "spec helper" do
+	before :each do
+		@fixture = cp_fixture_to_test_temp("test_fixture.txt")
+	end
+	
+	it "should do the right thing with copying a fixture" do		
+		expect(@fixture).to be_a_file_at_path_containing_text("test_temp/test_fixture.txt", "A File!")
+	end
+
+	it "should remove test temp directory entirely" do
+	 	clean_test_temp
+	 	expect(test_temp).to no_longer_exist
+	end
+end
+
+
 describe IntegrateJ2objc::J2ObjcSharedLibSmanger do  
 
-  it "it works exactly like before these infernal tests" do
-    
-  end
+	before :each do
+		clean_test_temp
+	end
 
+	it "it works exactly like before these infernal tests" do
+		
+	end
+
+
+end
+
+RSpec::Matchers.define :no_longer_exist do 
+	match do |file|
+		expect(File.exists?(file)).to be_falsey
+	end
+end
+
+RSpec::Matchers.define :be_a_file_at_path_containing_text do |path, text|
+	match do |file|
+		expect(file).to(end_with(path)) &&
+		expect(File.exists?(file)).to(be_truthy) &&
+		expect(File.read(file)).to(eql(text))
+	end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,34 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'j2objc_shared_lib_smanger'
+require 'fileutils'
+
+def fixture_dir
+	File.join(File.dirname(__FILE__), "fixture")
+end
+
+def test_temp
+	File.join(fixture_dir, "test_temp")
+end
+
+def ensure_test_temp
+	FileUtils.mkdir_p(test_temp)
+end
+
+def clean_test_temp
+	FileUtils.rm_r(test_temp)
+end
+
+def fixture_path(fixture)
+	File.join(fixture_dir, fixture)
+end
+
+def temp_fixture_copy_path(fixture)
+	File.join(test_temp, fixture)
+end
+
+def cp_fixture_to_test_temp(fixture)
+	ensure_test_temp
+	FileUtils.cp_r(fixture_path(fixture), test_temp)
+	temp_fixture_copy_path(fixture)
+end
+
