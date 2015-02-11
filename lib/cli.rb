2bba4fad3cd87d2b004bdf11d295b587a353e3e1
Jon Nolen <jon.nolen@gmail.com>
2015-02-11 09:45:58 -0500

    updating the options descriptions.

--- lib/cli.rb
+++ lib/cli.rb
@@ -11,18 +11,32 @@ module IntegrateJ2objc
 		%[ from XCODEPROJ and then adds all directories and files from SOURCE_ROOT, recursively, to] +
 		%[ the GROUP and TARGET]
 		
-		method_option :xcodeproj, required: true, type: :string, aliases: "-x",
-			desc: "relative path to xcode proj"
-		method_option :source_root, required: true, type: :string, aliases: "-s"
-		method_option :group, required: true, type: :string, aliases: "-g", 
-			desc: "This is the path to the group you are targeting from the xcode project, in Xcode"+
+		method_option :xcodeproj,
+			required: true,
+			type: :string,
+			aliases: "-x",
+			desc: "relative or absolute path to yourproject.xcodeproj, including yourproject.xcodeproj"
+		method_option :source_root, 
+			required: true, 
+			type: :string, 
+			aliases: "-s", 
+			desc: "the relatitve path from yourproject.xcodeproj to the generated sources directory."
+		method_option :group,
+			required: true,
+			type: :string,
+			aliases: "-g", 
+			desc: 
+			"This is the path to the group you are targeting from the xcode project, in Xcode"+
 			" For example in Xcode Project Navigator you see:\n"+
-			"\t\t\t\tMyProject.xcodeproj\n"+
-			"\t\t\t\t  \u2514 MyProject\n"+
-			"\t\t\t\t      \u2514 generated\n" +
-			"\t\t\t\twould have a group argument: -g \"MyProject/generated\""
-				
-		method_option :target, required: true, type: :string, aliases: "-t"		
+			"\t\t\t\t   MyProject.xcodeproj\n"+
+			"\t\t\t\t    \u2514 MyProject\n"+
+			"\t\t\t\t        \u2514 generated\n" +
+			"\t\t\t\t   would have a group argument: -g \"MyProject/generated\""				
+		method_option :target, 
+			required: true, 
+			type: :string, 
+			aliases: "-t",
+			desc: "the name of the target that generated sources should be added to in your xcode project"
 		def integrate_source()
 			J2ObjcSharedLibSmanger.new().integrate_source(options);
 		end
